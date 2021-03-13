a298c3bd69440421b879669411f7e8848c04401e
Karol Żebrowski <19karol98@gmail.com>
2021-03-13 15:52:21 +0100

    Server setup for heroku

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { history } from "./history";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://exchange-remitly-api.herokuapp.com/",
 });
 
 instance.interceptors.response.use(
